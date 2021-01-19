e67c2631f26589d5c2797361459e3def3f478e70
jephrae <jeff.simons@gmail.com>
2021-01-19 17:44:24 -0600

    adding details

--- dnd-monsters/src/Components/MonsterDetails.jsx
+++ dnd-monsters/src/Components/MonsterDetails.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as GiIcons from "react-icons/gi";
 
 export default function MonsterDetails(props) {
   const [monster, setMonster] = useState(null);
@@ -8,7 +10,6 @@ export default function MonsterDetails(props) {
       .then((res) => res.json())
       .then((res) => setMonster(res))
       .catch(console.error);
-    console.log(monster);
   }, [props.match.params.id]);
 
   if (!monster) {
@@ -16,9 +17,36 @@ export default function MonsterDetails(props) {
   }
 
   return (
-    <div>
-      <h3> monster deets test</h3>
-      {monster.name}
+    <div className="monster-container">
+      <div className="monster-name">
+        <h2> {monster.name}</h2>
+      </div>
+
+      <div className="armorclass">
+        {" "}
+        <GiIcons.GiChestArmor />
+        {monster.armor_class}
+      </div>
+      <div className="hitpoints">
+        <FaIcons.FaRegHeart />
+        {monster.hit_points}
+      </div>
+      <div className="hitdice">
+        <GiIcons.GiDiceTwentyFacesTwenty />
+        {monster.hit_dice}
+      </div>
+      <div className="speed">
+        <GiIcons.GiRun />
+        {monster.speed.walk}
+      </div>
+      <div className="attributes">
+        STR: {monster.strength}
+        DEX: {monster.dexterity}
+        CONST: {monster.constitution}
+        INT: {monster.intelligence}
+        WIS: {monster.wisdom}
+        CHR: {monster.charisma}
+      </div>
     </div>
   );
 }
