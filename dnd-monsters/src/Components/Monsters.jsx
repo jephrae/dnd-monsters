e09a9cc8bdc916fb5fdddec72d2b740b2e34f2de
jephrae <jeff.simons@gmail.com>
2021-01-19 10:50:37 -0600

    list working again with loading wait thing

--- dnd-monsters/src/Components/Monsters.jsx
+++ dnd-monsters/src/Components/Monsters.jsx
@@ -1,38 +1,79 @@
 import React, { useState, useEffect } from "react";
 // import { Link } from "react-router-dom";
 
-export default function Monsters(props) {
+const url = "https://www.dnd5eapi.co/api/monsters";
+
+export default function Monsters() {
   const [monsters, setMonsters] = useState([]);
 
-  useEffect(() => {
-    const url = "https://www.dnd5eapi.co/api/monsters";
+  const getMonsters = async () => {
+    const response = await fetch(url);
+    const monsters = await response.json();
+    setMonsters(monsters);
+    console.log(monsters);
+    console.log(monsters.results);
+  };
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((json) => {
-        setMonsters([json]);
-        console.log(json);
-      })
-      .catch(console.error);
+  useEffect(() => {
+    getMonsters();
   }, []);
 
-  if (!monsters) {
-    return <h2>loading...</h2>;
+  const monsterData = monsters.results;
+  // console.log(Object.keys(monsters.results));
+  if (monsters.length < 1) {
+    return <h3>loading...</h3>;
   }
-
-  console.log(monsters.name);
-
   return (
-    <section className="container">
-      {monsters.map((monsters) => {
-        return (
-          <div className="monsters" key={monsters.index}>
-            <p>{monsters.name}</p>
-          </div>
-        );
-      })}
-
-      <p>monster {monsters.index} test</p>
-    </section>
+    <>
+      <h3>monsters</h3>
+      <ul className="monster-list">
+        {monsterData.map((monsters, index) => {
+          // const { name, index } = monster;
+          // console.log(monsters.results);
+          return <li key={index}>{monsters.name}</li>;
+        })}
+      </ul>
+    </>
   );
 }
+
+// export default function Monsters() {
+//   const [monsters, setMonsters] = useState([]);
+
+//   useEffect(() => {
+//     const url = "https://www.dnd5eapi.co/api/monsters";
+
+//     fetch(url)
+//       .then((res) => res.json())
+//       .then((json) => {
+//         setMonsters([json]);
+//         console.log(monsters);
+//       })
+//       .catch(console.error);
+//   }, []);
+
+//   console.log(monsters);
+//   if (!monsters) {
+//     return <h2>loading...</h2>;
+//   }
+
+//   return (
+//     <div className="monster-container">
+//       {monsters.map((monster) => {
+//         console.log({ monsters });
+//         console.log({ monster });
+//         console.log(monsters);
+//         console.log(monster);
+//         console.log(monster.results);
+//         console.log(monster.results.name);
+
+//         return (
+//           <div className="monsters-list" key={monsters.index}>
+//             <ul> monster test </ul>
+//             <li key={monsters.index}> test {monster.name}</li>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
